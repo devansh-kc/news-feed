f4aba5e9330c8085051fdd058351b7c684ca2f63
devansh-kc <devansh.kc96@gmail.com>
2024-08-28 17:32:32 +0530

    revamped card

--- src/component/BlogPostItems.jsx
+++ src/component/BlogPostItems.jsx
@@ -11,49 +11,72 @@ import {
 function BlogPostItems({ newsData, pageStartCount }) {
   return (
     <>
-      {newsData
-        .slice(pageStartCount * 10 - 10, pageStartCount * 10)
-        .map((article, index) => (
-          <Grid key={index}>
-            <Card
-              sx={{
-                width: "370px",
-                maxWidth: "100%",
-              }}
-              color="neutral"
-              variant="outlined"
-              orientation="horizontal"
+      {newsData.map((article, index) => (
+        <div
+          key={index}
+          className="font-mono text-white m4 p-4  flex  justify-center items-center align-top  "
+        >
+          <div className=" p-4  m-4  ">
+            <img
+              src={article.urlToImage}
+              alt={article.title}
+              className="   object-cover"
+            />
+          </div>
+          <div className="break-all p-4 border border-white">
+            <h5> {article.title}</h5>
+            <p> {article.author ? article.author : ""}</p>
+            <p>
+              {" "}
+              {article.description && article.description.length > 100
+                ? `${article.description.substring(0, 300)}...`
+                : article.description || "No description available"}
+            </p>
+            <a href={article.url} target="_blank" rel="noopener noreferrer">
+              Read More
+            </a>
+          </div>
+        </div>
+        // <Grid key={index}>
+        //   <Card
+        //     sx={{
+        //       width: "370px",
+        //       maxWidth: "100%",
+        //     }}
+        //     color="neutral"
+        //     variant="outlined"
+        //     orientation="horizontal"
 
-              // invertedColors
-            >
-              <CardContent>
-                <AspectRatio sx={{ objectFit: "cover" }}>
-                  <img src={article.urlToImage} alt={article.title} />
-                </AspectRatio>
-                <Typography variant="h5" component="div">
-                  {article.title}
-                </Typography>
-                <Typography variant="subtitle1" color="text.secondary">
-                  {article.author ? article.author : "Unknown Author"}
-                </Typography>
-                <Typography variant="body2" color="text.primary">
-                  {article.description && article.description.length > 100
-                    ? `${article.description.substring(0, 100)}...`
-                    : article.description || "No description available"}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  <a
-                    href={article.url}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Read More
-                  </a>
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-        ))}
+        //     // invertedColors
+        //   >
+        //     <CardContent>
+        //       <AspectRatio sx={{ objectFit: "cover" }}>
+        //         <img src={article.urlToImage} alt={article.title} />
+        //       </AspectRatio>
+        //       <Typography variant="h5" component="div">
+        //         {article.title}
+        //       </Typography>
+        //       <Typography variant="subtitle1" color="text.secondary">
+        //         {article.author ? article.author : "Unknown Author"}
+        //       </Typography>
+        //       <Typography variant="body2" color="text.primary">
+        //         {article.description && article.description.length > 100
+        //           ? `${article.description.substring(0, 100)}...`
+        //           : article.description || "No description available"}
+        //       </Typography>
+        //       <Typography variant="body2" color="text.secondary">
+        //         <a
+        //           href={article.url}
+        //           target="_blank"
+        //           rel="noopener noreferrer"
+        //         >
+        //           Read More
+        //         </a>
+        //       </Typography>
+        //     </CardContent>
+        //   </Card>
+        // </Grid>
+      ))}
     </>
   );
 }
--- src/component/BlogPostList.jsx
+++ src/component/BlogPostList.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import BlogPostItems from "./BlogPostItems";
 import axios from "axios";
 import { Grid } from "@mui/joy";
-import Pagination from "./Pagination";
 
 function BlogPostList() {
   const [newsData, setNewsData] = useState([]);
@@ -10,7 +9,7 @@ function BlogPostList() {
   const [page, setPage] = useState(1);
 
   async function fetchNewsFromApi() {
-    const URL = `https://newsapi.org/v2/everything?q=india&apiKey=a08f2a51199e446db23bdbfd57b9f2e0&pageSize=41`;
+    const URL = `https://newsapi.org/v2/everything?q=tech&apiKey=a08f2a51199e446db23bdbfd57b9f2e0`;
     try {
       const result = await axios.get(URL);
       const storedData = result.data.articles;
@@ -30,43 +29,19 @@ function BlogPostList() {
     fetchNewsFromApi();
   }, []);
 
-  function selectPageHandler(event) {
-    setPage(event);
-  }
+ 
   return (
     <div className="">
       <Grid
         container
-        spacing={5}
         sx={{ flexGrow: 1 }}
         justifyContent="center"
         alignItems="flex-start"
       >
-        <BlogPostItems newsData={newsData} pageStartCount={page} />
+        
+        <BlogPostItems newsData={newsData} />
       </Grid>
-      <div className="flex items-center  justify-center p-6  ">
-        <span
-          className="mx-1  text-sm font-semibold text-gray-900"
-          onClick={(i) => selectPageHandler(page - 1)}
-        >
-          👈
-        </span>
-        {[...Array(newsData?.length / page)].map((_, i) => (
-          <span
-            onClick={() => selectPageHandler(i - 1)}
-            className="mx-1 flex items-center rounded-md border border-gray-400 px-3 py-1 text-white hover:scale-105"
-            key={i}
-          >
-            {i + 1}
-          </span>
-        ))}
-        <span
-          className="mx-2 text-sm font-semibold text-gray-900"
-          onClick={() => selectPageHandler(page + 1)}
-        >
-          👉
-        </span>
-      </div>
+    
     </div>
   );
 }
